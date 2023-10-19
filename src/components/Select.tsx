171eef6d129515618d47f02c044f4346d7ecadcc
romgranot <romgranot@mail.tau.ac.il>
2023-10-19 13:53:46 +0300

    finished multiple select logic

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,44 +1,106 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./select.module.css"
 
-type SelectOptions = {
+export type SelectOptions = {
     label: string;
     value: string | number;
 }
 
-type SelectProps =  {
-    options: SelectOptions[];
+type MultipleSelectProps = {
+    multiple: true;
+    onChange: (value: SelectOptions[]) => void;
+    value: SelectOptions[];
+}
+
+type SingleSelectProps = {
+    multiple?: false;
     onChange: (value: SelectOptions | undefined) => void;
     value?: SelectOptions;
 }
 
-export function Select({value, onChange, options}: SelectProps){
+type SelectProps =  {
+    options: SelectOptions[];
+} & (SingleSelectProps | MultipleSelectProps)
+
+export function Select({multiple, value, onChange, options}: SelectProps){
     const [isOpen, setIsOpen] = useState(false);
     const [highlightedIndex, setHighlightedIndex] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null)
 
     function clearOptions(){
-        onChange(undefined);
+        multiple ? onChange([]) : onChange(undefined);
     }
 
     function selectOption(option : SelectOptions){
-        if (option !== value) onChange(option);
+        if (multiple){
+            if (value.includes(option)) {
+                onChange(value.filter(o => o !== option));
+            } else {
+                onChange([...value, option]);
+            }
+        } else if (option !== value) onChange(option);
     }
 
     function isOptionSelected(option : SelectOptions){
-        return option === value;
+        return multiple ? value.includes(option) : option === value;
     }
 
     useEffect(() => {
         if (isOpen) setHighlightedIndex(0)
     }, [isOpen])
 
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.target != containerRef.current) return
+            switch(e.code) {
+                case "Enter":
+                case "Space":
+                    setIsOpen(prev => !prev)
+                    if (isOpen) selectOption(options[highlightedIndex])
+                    break;
+                case "ArrowUp":
+                case "ArrowDown": {
+                    if (!isOpen){
+                        setIsOpen(true);
+                        break;
+                    }
+                    const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+                    if (newValue >= 0 && newValue < options.length) {
+                        setHighlightedIndex(newValue);
+                    }
+                    break;
+                }
+                case "Escape":
+                    setIsOpen(false);
+                    break;
+            }
+        };
+        containerRef.current?.addEventListener("keydown", handler);
+        return () => {
+            containerRef.current?.removeEventListener("keydown", handler);
+        }
+    }, [isOpen, highlightedIndex, options])
+
     return (
         <div 
+            ref={containerRef}
             onClick={() => setIsOpen(prev => !prev)} 
             onBlur={() => setIsOpen(false)}
             tabIndex={0} 
             className={styles.container}>
-            <span className={styles.value}>{value?.label}</span>
+            <span className={styles.value}>{multiple 
+                ? value.map(v => (
+                <button 
+                    key={v.value} 
+                    onClick={e => {
+                        e.stopPropagation();
+                        selectOption(v);
+                    }} className={styles["option-badge"]}
+                    >
+                        {v.label}
+                        <span className={styles["remove-btn"]}>&times;</span></button>
+            )) 
+                : value?.label}</span>
             <button 
                 onClick={e => {
                     e.stopPropagation();
